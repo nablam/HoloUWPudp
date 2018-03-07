3da0ba0a9df602a1e6c09d3245dbd5b3d8a7965e
unknown <lamribenn@gmail.com>
2018-03-06 22:55:32 -0500

    client side works

---
+++ Assets/Scripts/UDP_Stuff/udpSendKeyboardHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class udpSendKeyboardHelper : MonoBehaviour {
+    public UDPCommunication _coomm;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            _coomm.SendUDPMessage(_coomm.externalIP, _coomm.externalPort, Encoding.UTF8.GetBytes(_coomm.PingMessage));
+        }
+    }
+
+}
