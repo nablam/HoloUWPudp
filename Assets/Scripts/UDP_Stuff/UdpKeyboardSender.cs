626776d762dbe729d29c0badeee8e59a7b9c3a12
nablam <lamribenn@gmail.com>
2018-03-06 23:17:30 -0500

    ignore and ready for 2 pc connection

---
+++ Assets/Scripts/UDP_Stuff/UdpKeyboardSender.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Text;
+using UnityEngine;
+
+public class UdpKeyboardSender : MonoBehaviour {
+
+    public UDPCommunication UdpCOMM;
+    public TextDisplay TextDisplayer;
+
+    int cnt = 0;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            cnt++;
+            UdpCOMM.SendUDPMessage(UdpCOMM.externalIP, UdpCOMM.externalPort, Encoding.UTF8.GetBytes(UdpCOMM.PingMessage + cnt.ToString()));
+        }
+    }
+}
