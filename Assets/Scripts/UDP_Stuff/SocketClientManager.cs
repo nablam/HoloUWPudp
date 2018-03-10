5e87b17e14688d74c81b01e2c389f798fab7689d
nablam <lamribenn@gmail.com>
2018-03-10 02:57:52 -0500

    working

--- Assets/Scripts/UDP_Stuff/SocketClientManager.cs
+++ Assets/Scripts/UDP_Stuff/SocketClientManager.cs
@@ -1,233 +1,235 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Net;
-using System.Net.Sockets;
-using System.Threading;
-
-
-public class SocketClientManager
+using System.IO;
+#if !UNITY_EDITOR
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+#endif
+namespace MixedRealityNetworking
 {
-    #region Fields
-
-    /// <summary>
-    /// The ip address the client connects to
-    /// </summary>
-    private static string host;
-
-    /// <summary>
-    /// The port the client connects to
-    /// </summary>
-    private static int port;
-
-    /// <summary>
-    /// Contains the callbackmethods for seperate message ID's
-    /// </summary>
-    private static Dictionary<byte, Action<NetworkMessage>> callbackMethods = new Dictionary<byte, Action<NetworkMessage>>();
-
-    /// <summary>
-    /// The thread on which the socket runs
-    /// </summary>
-    private static Thread socketThread;
-
-    /// <summary>
-    /// The UDP client
-    /// </summary>
-    private static UdpClient udpClient;
-
-    /// <summary>
-    /// Making sure the thread aborts
-    /// </summary>
-    private static bool abortThread = false;
-
-    /// <summary>
-    /// Boolean that indicates if we should print debug information
-    /// </summary>
-    private static bool verboseMode = false;
-
-    #endregion
-
-    #region Properties
-
-    /// <summary>
-    /// The host to which the socket needs to connect
-    /// </summary>
-    public static string Host
+    public class SocketClientManager
     {
-        set { SocketClientManager.host = value; }
-    }
+#region Fields
 
-    /// <summary>
-    /// The port to which the socket needs to connect
-    /// </summary>
-    public static int Port
-    {
-        set { SocketClientManager.port = value; }
-    }
+        /// <summary>
+        /// The ip address the client connects to
+        /// </summary>
+        private static string host;
 
-    /// <summary>
-    /// Boolean indicating if debug info should be printed
-    /// </summary>
-    public static bool VerboseMode
-    {
-        get { return SocketClientManager.verboseMode; }
-        set { SocketClientManager.verboseMode = value; }
-    }
+        /// <summary>
+        /// The port the client connects to
+        /// </summary>
+        private static int port;
 
-    #endregion
+        /// <summary>
+        /// Contains the callbackmethods for seperate message ID's
+        /// </summary>
+        private static Dictionary<byte, Action<NetworkMessage>> callbackMethods = new Dictionary<byte, Action<NetworkMessage>>();
 
-    #region Methods
+#if !UNITY_EDITOR
+        /// <summary>
+        /// The socket
+        /// </summary>
+        private static DatagramSocket udpSocket;
+#endif
+        /// <summary>
+        /// Boolean that indicates if we should print debug information
+        /// </summary>
+        private static bool verboseMode = false;
 
-    /// <summary>
-    /// Connects to the <c cref="udpClient">socket</c> and starts listening
-    /// </summary>
-    public static void Connect()
-    {
-        SocketClientManager.udpClient = udpClient = new UdpClient(SocketClientManager.host, SocketClientManager.port);
+#endregion
 
-        SocketClientManager.socketThread = new Thread(SocketClientManager.Listen);
-        SocketClientManager.socketThread.Start();
-    }
+#region Properties
 
-    /// <summary>
-    /// Method to subscribe to a message
-    /// </summary>
-    /// <exception cref="InvalidOperationException">Gets thrown when there is already a subscription for the message ID</exception>
-    /// <param name="messageId">The id of the message you want to subscribe to</param>
-    /// <param name="callbackMethod">The method that should be called</param>
-    public static void Subscribe(byte messageId, Action<NetworkMessage> callbackMethod)
-    {
-        // Check if not already subscribed
-        if (SocketClientManager.callbackMethods.ContainsKey(messageId))
-            throw new InvalidOperationException("There is already a subscription to this message ID");
-
-        SocketClientManager.callbackMethods.Add(messageId, callbackMethod);
-    }
-
-    /// <summary>
-    /// Sends a message
-    /// </summary>
-    /// <param name="nm">The <c cref="NetworkMessage">network message</c> that needs to be send</param>
-    public static void SendMessage(NetworkMessage nm)
-    {
-        // Write the data into a byte array
-        byte[] byteArray = new byte[nm.Content.Length + 1];
-
-        byteArray[0] = nm.MessageId;
-
-        // Write the content into the array
-        var i = 1;
-
-        foreach (byte messageData in nm.Content)
+        /// <summary>
+        /// The host to which the socket needs to connect
+        /// </summary>
+        public static string Host
         {
-            byteArray[i] = messageData;
-            ++i;
+            set { SocketClientManager.host = value; }
         }
 
-        PrintDebug("Sending message");
-
-        // Send it
-        udpClient.Send(byteArray, byteArray.Length);
-    }
-
-    /// <summary>
-    /// Method that listens for incoming messages on the <c cref="udpClient">socket</c>
-    /// </summary>
-    private static void Listen()
-    {
-        try
+        /// <summary>
+        /// The port to which the socket needs to connect
+        /// </summary>
+        public static int Port
         {
-            IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, 0);
+            set { SocketClientManager.port = value; }
+        }
 
-            // Keep looping as long as the thread isn't aborted
-            while (!SocketClientManager.abortThread)
+        /// <summary>
+        /// Boolean indicating if debug info should be printed
+        /// </summary>
+        public static bool VerboseMode
+        {
+            get { return SocketClientManager.verboseMode; }
+            set { SocketClientManager.verboseMode = value; }
+        }
+
+#endregion
+
+#region Methods
+
+        /// <summary>
+        /// Connects to the socket and starts listening
+        /// </summary>
+        public static void Connect()
+        {
+ #if !UNITY_EDITOR
+            SocketClientManager.udpSocket = new DatagramSocket();
+            SocketClientManager.udpSocket.MessageReceived += SocketClientManager.MessageReceived;
+
+            // Calling the binding async
+            SocketClientManager.Bind();
+#endif
+        }
+
+#if !UNITY_EDITOR
+        /// <summary>
+        /// Binds to an endpoint
+        /// This is not done in the Connect() method, because it's async
+        /// and Unity may have problems with it...
+        /// </summary>
+        private static async void Bind()
+        {
+            await SocketClientManager.udpSocket.BindServiceNameAsync(SocketClientManager.port.ToString());
+        }
+#endif
+        /// <summary>
+        /// Method to subscribe to a message
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Gets thrown when there is already a subscription for the message ID</exception>
+        /// <param name="messageId">The id of the message you want to subscribe to</param>
+        /// <param name="callbackMethod">The method that should be called</param>
+        public static void Subscribe(byte messageId, Action<NetworkMessage> callbackMethod)
+        {
+            // Check if not already subscribed
+            if (SocketClientManager.callbackMethods.ContainsKey(messageId))
+                throw new InvalidOperationException("There is already a subscription to this message ID");
+
+            SocketClientManager.callbackMethods.Add(messageId, callbackMethod);
+        }
+
+#if !UNITY_EDITOR
+        /// <summary>
+        /// Sends a message
+        /// </summary>
+        /// <param name="nm">The <c cref="NetworkMessage">network message</c> that needs to be send</param>
+        public static void SendMessage(NetworkMessage nm)
+        {
+            // Write the data into a byte array
+            byte[] byteArray = new byte[nm.Content.Length + 1];
+
+            byteArray[0] = nm.MessageId;
+
+            // Write the content into the array
+            var i = 1;
+
+            foreach (byte messageData in nm.Content)
             {
-                byte[] clientData = udpClient.Receive(ref endpoint);
+                byteArray[i] = messageData;
+                ++i;
+            }
 
-                // Data received, create a new NetworkMessage
-                byte messageId = clientData[0];
+            PrintDebug("Sending message");
 
-                // Remove the message ID from the data
-                byte[] message = new byte[clientData.Length - 1];
+            SocketClientManager.SendMessageToSocket(byteArray);
+        }
+#endif
+#if !UNITY_EDITOR
+        /// <summary>
+        /// Internal method for sending the message to the socket
+        /// Because this method is async, it has to be in a seperated method
+        /// </summary>
+        /// <param name="byteArray">The network message converted to a byte array</param>
+        private async static void SendMessageToSocket(byte[] byteArray)
+        {
+            IOutputStream streamOut = (await SocketClientManager.udpSocket.GetOutputStreamAsync(new Windows.Networking.HostName(SocketClientManager.host), SocketClientManager.port.ToString()));
 
-                for (int i = 1; i < clientData.Length; ++i)
-                {
-                    message[i - 1] = clientData[i];
-                }
-
-                // Call the correct callback method
-                if (SocketClientManager.callbackMethods.ContainsKey(messageId))
-                {
-                    // Catch any exceptions and rethrow them,
-                    // so a user gets a good exception instead of a 
-                    // object disposed exception
-                    try
-                    {
-                        SocketClientManager.callbackMethods[messageId](new NetworkMessage(messageId, message));
-                    }
-                    catch (Exception e)
-                    {
-                        throw e;
-                    }
-                }
-                else
-                {
-                    PrintDebug("No known callback for message ID " + messageId.ToString());
-                }
+            // Write the data
+            using (DataWriter writer = new DataWriter(streamOut))
+            {
+                writer.WriteBytes(byteArray);
+                await writer.StoreAsync();
             }
         }
-        catch (ThreadAbortException e)
-        {
-            // Thread aborted, do nothing
-        }
-        catch (SocketException e)
-        {
-            // Socket aborted, probably because we want to close it
-            if (e.ErrorCode != 10004)
-                throw e;
-        }
-        catch (Exception e)
-        {
-            // Rethrow exception
-            throw e;
 
-            PrintDebug(e.Message);
-        }
-        finally
+
+
+        /// <summary>
+        /// Event handler that gets called when a message is received on the socket
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="data"></param>
+        private static void MessageReceived(Windows.Networking.Sockets.DatagramSocket sender, Windows.Networking.Sockets.DatagramSocketMessageReceivedEventArgs args)
         {
-            // Make sure we always close the connection
-            udpClient.Close();
+            Stream streamIn = args.GetDataStream().AsStreamForRead();
+
+            byte[] byteArray;
+
+            // Use the memory stream to convert the stream to a byte array
+            using (var memoryStream = new MemoryStream())
+            {
+                streamIn.CopyTo(memoryStream);
+                byteArray = memoryStream.ToArray();
+            }
+
+            // Data received, create a new NetworkMessage
+            byte messageId = byteArray[0];
+
+            // Remove the message ID from the data
+            byte[] message = new byte[byteArray.Length - 1];
+
+            for (int i = 1; i < byteArray.Length; ++i)
+            {
+                message[i - 1] = byteArray[i];
+            }
+
+            // Build network message
+            NetworkMessage nm = new NetworkMessage(messageId, message);
+
+            // Call the correct callback method
+            if (SocketClientManager.callbackMethods.ContainsKey(messageId))
+            {
+                // Catch any exceptions and rethrow them,
+                // so a user gets a good exception instead of a 
+                // object disposed exception
+                try
+                {
+                    SocketClientManager.callbackMethods[messageId](nm);
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+            else
+            {
+                PrintDebug("No known callback for message ID " + messageId.ToString());
+            }
         }
+
+        /// <summary>
+        /// Stops listening to the socket and closes the thread
+        /// </summary>
+        public static void StopListening()
+        {
+            // Close the UDP client
+            if (udpSocket != null)
+                udpSocket.Dispose();
+        }
+#endif
+        /// <summary>
+        /// Method that prints debug information when verbose mode is enabled
+        /// </summary>
+        /// <param name="message">The message that needs to be printed</param>
+        private static void PrintDebug(string message)
+        {
+            if (SocketClientManager.verboseMode)
+                Debug.WriteLine(message);
+        }
+
+#endregion
     }
-
-    /// <summary>
-    /// Stops listening to the <c cref="udpClient">socket</c> and closes the <c cref="socketThread">thread</c>
-    /// </summary>
-    public static void StopListening()
-    {
-        // Close the UDP client, since aborting the thread
-        // doesn't work if it's waiting for packets
-        if (udpClient != null)
-            udpClient.Close();
-
-        // Check if the thread is initialized
-        if (SocketClientManager.socketThread is Thread && SocketClientManager.socketThread != null)
-        {
-            SocketClientManager.abortThread = true;
-
-            //SocketClientManager.socketThread.Abort();
-        }
-    }
-
-    /// <summary>
-    /// Method that prints debug information when verbose mode is enabled
-    /// </summary>
-    /// <param name="message">The message that needs to be printed</param>
-    private static void PrintDebug(string message)
-    {
-        if (SocketClientManager.verboseMode)
-            Trace.Write(message);
-    }
-
-    #endregion
 }
---
+++ Assets/Scripts/UDP_Stuff/UdpLiseteningObj.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using UnityEngine;
+using System.Linq;
+using MixedRealityNetworking;
+
+public class UdpLiseteningObj : MonoBehaviour {
+     
+    // Use this for initialization
+     TextDisplay TextDisplayer;
+    public TextMesh textMeshBox;
+#if !UNITY_EDITOR
+    Action<NetworkMessage> Showit;
+#endif
+    byte _thIdtolistento;
+    void Awake () {
+        int x = 123;
+        _thIdtolistento = Convert.ToByte(x);
+
+#if !UNITY_EDITOR
+        Showit=DotheShow;
+        SocketClientManager.Port = 12346;
+        SocketClientManager.Host = "192.168.1.8";
+        SocketClientManager.Connect();
+        SocketClientManager.Subscribe(_thIdtolistento, DotheShow);
+#endif
+    }
+    private void OnEnable()
+    {
+        TextDisplayer = GetComponent<TextDisplay>();
+
+    }
+    string mesg = "";
+#if !UNITY_EDITOR
+
+    void DotheShow(NetworkMessage argMessage) {
+
+        //TextDisplayer.DisplayToTextMesh(argMessage.Content.ToString());
+      //  TextDisplayer.DisplayToTextMesh("dddd");
+        mesg=System.Text.Encoding.UTF8.GetString(argMessage.Content);
+      //  if (textMeshBox != null)
+        //    textMeshBox.text = "dddd";// System.Text.Encoding.UTF8.GetString(argMessage.Content);
+    }
+#endif
+    // Update is called once per frame
+    void Update () {
+      
+        textMeshBox.text = mesg;
+    }
+}
